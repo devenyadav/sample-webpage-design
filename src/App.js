c28fe19df322464f6e2cc37992c92ea26748cc26
yadavdeven <devenyadav0913@gmail.com>
2022-11-23 21:29:24 +0545

    commit

---
+++ src/App.js
@@ -0,0 +1,389 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+<div div className="task">
+    <header>
+
+      <div className="wrapper">
+        <nav>
+          <div className="logo">AR Shakir</div>
+          <ul>
+            <li>
+              <select>
+                <option>Product</option></select>
+            </li>
+            <li>
+              <select><option>Templete</option></select>
+            </li>
+            <li>
+              <a href="/">Blog</a>
+            </li>
+
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+          </ul>
+
+          <div className="left">
+           
+            <a href=""> Sign in</a>
+            <button className="btn">Start Free</button>
+            
+          </div>
+        </nav>
+        {/* ending of navbar */}
+
+        <div className="hero-section">
+          <div className="left-desc">
+            <h1> Managing freelance<div>payments has never</div>been easier</h1>
+            <p>Yet bed any for travelling assistance indulgence<div> unpleasing. Not thoughts all exercise blessing.</div>
+             </p>
+             <div className="getstart">
+            <div >
+              <button className="btn-rectangle">Get Started</button>
+             
+              </div>
+            < div className="btn-circle">
+              <img src=".\images\Group 176.png" />
+              <p>See How It Works</p>
+            </div>
+            </div>
+            <div className="register">
+              <img src=".\images\Group 155.png" />
+              <p>Free Register</p>
+              <img src=".\images\Group 155.png" />
+              <p>Great Service</p>
+
+
+            </div>
+
+          </div>
+          <div className="right">
+            <img src=".\images\Group 686.png"  className="image1"/>
+           
+
+          </div>
+       </div>
+      </div>
+    </header>
+
+    {/* end of hearder section */}
+    {/* start of sponser */}
+    <div className="sponser">
+      <h3 className="sponser-heading">Over 32k+ software  businesses growing with AR Shakir.</h3>
+      <div className="partnership">
+      <img src=".\images\fimage.png" />
+      <img src=".\images\Vector (1).png" />
+      <img src=".\images\Group2x.png" />
+      <img src=".\images\Vector (3).png" />
+      <img src=".\images\Group.jpg" />
+      <img src=".\images\Vector (4).png" />
+      <img src=".\images\Vector (5).png" />
+      </div>
+    </div>
+    {/* end of sponser */}
+    {/* start of protofolio */}
+    <div className="protofolio">
+      <div className="tracking">
+        <h3>Automated
+<div>portfolio tracking</div></h3>
+<p>Yet bed any for travelling assistance indulgence.<div>Not thoughts all exercise blessing.</div> </p>
+      </div>
+      <div className="description">
+      <img src=".\images\double.png" />
+        <p>Wise busy past both park when an ye no. Nay <div>likely her length sooner thrown sex lively income.</div><div> The expense windows adapted sir. Wrong widen</div>.
+        <div>- Mike Taylor, Web Designer</div></p>
+         </div>
+    </div>
+
+
+<div className="invoice">
+  <div className="wallet">
+   
+    <h2>01</h2>
+   <div className="bkimg">
+    <img src=".\images\blue.png"/> 
+    <img src=".\images\black.png" className="img3"/></div>
+    <h3> Connect wallets & companies</h3>
+    <p>Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of. </p>
+  </div>
+  <div className="transaction">
+    <h2>02</h2>
+    <div className="bkimg">
+    <img src=".\images\orange.png"/>
+    <img src=".\images\black.png" className="img3"/></div>
+    <h3>Review
+transactions</h3>
+<p>Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of. </p>
+  </div>
+  <div className="income">
+    <h2>03</h2>
+    <div className="bkimg">
+     <img src=".\images\yellow.png"/> 
+    <img src=".\images\black.png"  className="img3"/> </div>
+      <h3>Get income
+insights</h3>
+    
+    <p>Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of. </p>
+  </div>
+</div>
+{/* end of protofolio */}
+
+{/* start of contents */}
+<div className="contents">
+  <div className="reviewer">
+    <img src=".\images\Group 4362.png" />
+  </div>
+  <div className="selection">
+    <div className="payment">
+      <p>Why Choose Us</p>
+    <h3>Track your payments<div></div>
+on the go with the<div></div>
+best way possible</h3>
+<p> Indulgence way everything joy alteration boisterous<div></div> the attachment. Party we years to order allow asked of. </p>
+</div>
+<div className="orange-tick">
+<img src=".\images\otick.png" />
+<p className="text">Get Overview at a glance</p>
+<img src=".\images\otick.png" />
+<p className="text">Deoposit funds easily, securlity</p>
+<img src=".\images\otick.png" />
+<p className="text">Get Live Support</p>
+</div>
+  </div>
+</div>
+
+
+ {/* start of save money proper transaction */}
+<div className="save-money">
+  <div className="proper-transaction">
+    <p>Why Choose Us</p>
+    <h3>Save money with
+<div>proper transaction</div></h3>
+
+<p>We so opinion friends me message as delight. <div>Whole front do of plate heard oh ought. His </div>defective nor convinced residence own.</p>
+
+<div className="quotes" >
+<img src=".\images\double.png" />
+<h5>Wise busy past both park when an ye no. Nay likely her length <div>sooner thrown sex lively income. The expense windows .</div>
+ <p>- Mike Taylor, Web Designer</p></h5></div>
+  </div>
+  <div className="chart">
+  <img  src=".\images\Group 149.png" className="img1"/>
+    <img src=".\images\image 56.png" className="img2"/>
+  </div>
+</div>
+{/* end of save money proper transaction */}
+
+{/* start of features section */}
+<div className="rect">
+  <h1 className="heading">Features that blows mind</h1>
+ 
+ <p className="heading">We so opinion friends me message as delight. Whole front do of plate heard <div>oh ought. His defective nor convinced residence own.</div></p>
+<div className="features">
+  <div className="boy-law">
+  <div className="bkimage">
+    <img src=".\images\orange.png"/>
+    <img src=".\images\black.png" className="img4"/></div>
+    <h3>Really boy law county she <div>unable her sister</div></h3>
+    <p>We so opinion friends me message as<div>delight. Whole front do of plate heard oh ought</div> .</p>
+  </div>
+  <div className="leave-law">
+  <div className="bkimage">
+    <img src=".\images\blue.png"/>
+    <img src=".\images\black.png" className="img4"/></div>
+    <h3>Among sex are leave law<div></div> built now</h3>
+    <p>We so opinion friends me message as <div> delight.Whole front do of plate heard oh ought.</div></p>
+    
+    </div>
+    <div className="drawback">
+      <div className="desc1">Merits behind on afraid or<div>warmly</div> </div>
+      <div className="desc2">Believing neglected so so <div>allowance existence</div></div>
+    
+  </div>
+</div>
+</div>
+{/* end of features section */}
+
+{/* start of testemonial  */}
+<div className="testimonial">
+  <h1>Testimonials</h1>
+
+  <p>We so opinion friends me message as delight. Whole front do of plate heard oh <div></div>ought. His defective nor convinced residence own.</p>
+<div className="testimonial-review">
+
+<div className="girl-side">
+<div className="girl-boyquotes"><img src=".\images\double.png" /></div>
+  <p>Wise busy past both park when an ye no. Nay likely her length sooner thrown sex lively income. The expense windows . Blessing welcomed ladyship.- Mike Taylor, Web Designer</p>
+<img src=".\images\Rectangle 151.png"/>
+</div>
+<div className="boyside">
+<div className="girl-boyquotes"><img src=".\images\double.png" /></div>
+<p>Wise busy past both park when an ye no. Nay likely her length sooner thrown sex lively income. The expense windows . Blessing welcomed ladyship.- Mike Taylor, Web Designer</p>
+<img src=".\images\Rectangle 151 (1).png"/>
+</div>
+</div>
+</div>
+
+{/* end of testimonial */}
+
+{/* start of user information */}
+<div className="stats">
+  <p>We speak with<div></div>
+our powerfull<div></div>
+statistics
+</p> 
+<div className="user">
+ {/* <h1>1M+</h1> */}
+<img src=".\images\Group 323.png"/>
+{/* <p>Active Users</p> */}
+</div>
+<div className="reviews">
+  <h1>30K+</h1>
+  <img src=".\images\star 11.png"/>
+  <h4>5-Stars Reviews</h4>
+</div>
+<div className="transation">
+  <h1>$252M</h1>
+  <img src=".\images\star 11.png"/>
+  <h4>Transactions</h4>
+</div>
+</div>
+{/* end of user information */}
+
+
+{/* start of blogs section */}
+
+
+<div className="blogs">
+  <h1>Recent Blogs</h1>
+  <div className="row">
+    <div className="best-practices"><h5>Best Practices</h5>
+    <h2 className="row-h2">In design active temper be uneasy.<div></div>Thirty for remove plenty regard.</h2>
+    <h5 >Read more -></h5>
+    </div>
+   <div><img src=".\images\Rectangle 155.png" className="row-image"/></div>
+    </div>
+    </div>
+
+
+  <div className="col-practices">
+    <div className="col1">
+    <h5>Best Practices</h5>
+      <h2>Partiality on or continuing particular principles as. </h2>
+      
+    </div>
+<div className="col2">
+<h5>Best Practices</h5>
+<h2>Village did removed enjoyed explain</h2>
+
+</div>
+<div className="col3">
+<h5>Best Practices</h5>
+<h2>Wise busy past both park when an ye no. Nay likely her length.</h2>
+
+</div>
+ 
+</div>
+<div className="col-images">
+<img src=".\images\Rectangle 155 (1).png"/>
+<img src=".\images\Rectangle 155 (2).png"/>
+<img src=".\images\Rectangle 155 (3).png" className="img3"/>
+</div>
+
+{/* end of blogs section */}
+
+
+{/* start of faq */}
+
+<div className="question-section">
+  <h1>Fequently<div></div>
+asked questions</h1></div>
+
+<div className="faq"> 
+< div className="left-section">
+  < div className="first">
+  <div><h4>The expense windows adapted sir. Wrong widen drawn.</h4>
+  <p> Offending belonging promotion provision an be oh consulted ourselves it. Blessing welcomed ladyship she met humoured sir breeding her. </p></div>
+<h5>+</h5></div>
+<div className="second"><h4>Six curiosity day assurance bed necessary?</h4><h5>+</h5></div>
+<div className="third"><h4>Produce say the ten moments parties?</h4><h5>+</h5></div>
+<div className="fourth"><h4>Simple innate summer fat appear basket his desire joy?</h4><h5>+</h5></div>
+<div className="fifth"><h4>Outward clothes promise at gravity do excited?</h4><h5>+</h5></div>
+</div>
+
+<div className="explain">
+  <div className="morequestion">
+  <img src=".\images\Vector 355.png"/>
+    <h3>Do you have more questions?</h3>
+    <p>End-to-end payments and financial<div></div> management in a single solution. Meet the <div></div>right platform to help realize.</p>
+<button className="btn">Shoot a Direct Mail</button>
+  </div>
+</div>
+</div>
+{/* end of faq */}
+
+
+
+{/* start of discover */}
+<div className="discover">
+
+    <h2>Discover a better way to<div></div> manage spendings</h2>
+  <button className="btn">Get Started now</button>
+</div>
+{/* end of discover */}
+
+
+{/* start of footer section */}
+<div className="footer">
+  <div className="social">
+    <h3>AR Shakir</h3>
+    <h5>Finance helps companies<div></div> manage payments easily.</h5>
+    <div className="socialpartner">
+    <img src=".\images\052-linkedin.png"/>
+    <img src=".\images\Vector (6).png"/>
+    <img src=".\images\Vector (7).png"/>
+    <img src=".\images\Vector (8).png"/>
+    </div>
+</div>
+<div className="company">
+  <h3>Company</h3>
+  <h5>About Us</h5>
+  <h5>Careers</h5>
+  <h5>Blog</h5>
+  <h5>Pricing</h5>
+
+</div>
+<div className="resources">
+  <h3> Resources</h3>
+  <h5>Proposal Template</h5>
+  <h5>Invoice Template</h5>
+  <h5>Tuturoial</h5>
+  <h5>How to write a contract</h5>
+</div>
+<div className="joinus">
+  <h3>
+  Join Our Newsletter
+  </h3>
+<div className="form">
+  <form action="#">
+  <input type="text" placeholder="Your email address"/>
+  <button className="subs-btn">Subscribe</button>
+  </form>
+  </div>
+<p> Will send you weekly updates for your better<div></div>
+finance management.</p>
+</div>
+</div>
+
+{/* end of footer  */}
+
+<div className="copyright">Copyright @ AR Shakir 2022. All Rights Reserved.</div>
+</div>
+  );
+}
+
+export default App;
